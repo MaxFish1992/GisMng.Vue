02615b4fd73bae3ec36e2cb9c72b08d570329274
MaxFish1992 <348756718@qq.com>
2018-12-04 15:41:21 +0800

    lineChart

--- src/components/common/lineChart.vue
+++ src/components/common/lineChart.vue
@@ -12,25 +12,83 @@ export default {
     return {};
   },
   mounted() {
-    var myChart = this.$echarts.init(document.getElementById("lineChart"));
-    myChart.setOption({
-      title: {
-        text: "折线图"
-      },
-      xAxis: {
-        data: ["2015", "2016", "2017"]
-      },
-      yAxis: {},
-      series: [
-        {
-          name: "累计长度",
-          type: "line",
-          data: [10000, 20000, 30000]
-        }
-      ]
-    });
+    this.getLineLength();
   },
-  methods: {}
+  methods: {
+    initChart() {
+      debugger;
+      var myChart = this.$echarts.init(document.getElementById("lineChart"));
+      var years = []; //淹没区
+      var lengths = []; //影响区
+      this.LengthData.forEach(function(item, index) {
+        years.push(item.year);
+        lengths.push(item.length);
+      });
+      myChart.setOption({
+        title: {
+          text: "累计线路长度"
+        },
+        tooltip: {
+          trigger: "axis",
+          formatter: "{a0}:{c0}km"
+        },
+        legend: {
+          data: ["累计线路长度"]
+        },
+        grid: {
+          left: "15%", //组件距离容器左边的距离
+          right: "5%",
+          bottom: "7%"
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true }
+          }
+        },
+        calculable: true,
+        xAxis: [
+          {
+            type: "category",
+            boundaryGap: false,
+            // data: ["2015", "2016", "2017"]
+            data: years
+          }
+        ],
+        yAxis: [
+          {
+            type: "value",
+            axisLabel: {
+              formatter: "{value}km"
+            }
+          }
+        ],
+        series: [
+          {
+            name: "线路长度",
+            type: "line",
+            data: lengths
+            // markLine: {
+            //   data: [{ type: "average", name: "平均值" }]
+            // }
+          }
+        ],
+        color: ["rgb(103,194,58)"]
+      });
+    },
+    getLineLength() {
+      this.$fetch("/LineLength/GetAll").then(response => {
+        this.LengthData = response;
+        this.initChart();
+        console.log(this.LengthData);
+      });
+    },
+    getLineCapacity() {
+      this.$fetch("/LineCapacity/GetAll").then(response => {
+        console.log(response);
+      });
+    }
+  }
 };
 </script>
 
--- src/components/views/Gis.vue
+++ src/components/views/Gis.vue
@@ -9,11 +9,9 @@
       placement="right"
       trigger="click"
     >
-      <div
-        id="lineChart"
-        class="chart"
-        style="height:300px;width:430px"
-      ></div>
+      <div>
+        <lineChart ref="lineChart"></lineChart>
+      </div>
       <el-button
         icon="el-icon-arrow-right"
         type="info"
@@ -28,6 +26,7 @@
 import lineChart from "@/components/common/lineChart.vue";
 export default {
   name: "GIS",
+  components: { lineChart },
   data() {
     return {
       title: "GIS页面",
@@ -36,7 +35,6 @@ export default {
   },
   mounted() {
     this.initMap();
-    this.getLineLength();
   },
   methods: {
     initMap() {
@@ -69,79 +67,6 @@ export default {
       map.addLayer(l2);
       map.addLayer(l3);
     },
-    initChart() {
-      debugger;
-      var myChart = this.$echarts.init(document.getElementById("lineChart"));
-      var years = []; //淹没区
-      var lengths = []; //影响区
-      this.LengthData.forEach(function(item, index) {
-        years.push(item.year);
-        lengths.push(item.length);
-      });
-      myChart.setOption({
-        title: {
-          text: "累计线路长度"
-        },
-        tooltip: {
-          trigger: "axis",
-          formatter: "{a0}:{c0}km"
-        },
-        legend: {
-          data: ["累计线路长度"]
-        },
-        grid: {
-          left: "15%", //组件距离容器左边的距离
-          right: "5%",
-          bottom: "7%"
-        },
-        toolbox: {
-          show: true,
-          feature: {
-            mark: { show: true }
-          }
-        },
-        calculable: true,
-        xAxis: [
-          {
-            type: "category",
-            boundaryGap: false,
-            // data: ["2015", "2016", "2017"]
-            data: years
-          }
-        ],
-        yAxis: [
-          {
-            type: "value",
-            axisLabel: {
-              formatter: "{value}km"
-            }
-          }
-        ],
-        series: [
-          {
-            name: "线路长度",
-            type: "line",
-            data: lengths
-            // markLine: {
-            //   data: [{ type: "average", name: "平均值" }]
-            // }
-          }
-        ],
-        color: ["rgb(103,194,58)"]
-      });
-    },
-    getLineLength() {
-      this.$fetch("/LineLength/GetAll").then(response => {
-        this.LengthData = response;
-        this.initChart();
-        console.log(this.LengthData);
-      });
-    },
-    getLineCapacity() {
-      this.$fetch("/LineCapacity/GetAll").then(response => {
-        console.log(response);
-      });
-    }
   }
 };
 </script>
